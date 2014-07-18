318314e82fc36d27a39ca78b5ee7217d762f714b
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-18 14:23:44 -0600

    homepage

---
+++ app.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require "gschool_database_connection"
+require "rack-flash"
+
+class App < Sinatra::Application
+  enable :sessions
+  use Rack::Flash
+
+  def initialize
+    super
+    @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
+  end
+
+  get "/" do
+    erb :root
+  end
+
+end
